0dc1efcf32baee462012ae8f80bc85c6f9999fbc
onis20 <emem.c.onis@gmail.com>
2025-02-22 11:06:46 +0200

    investment code improvement

--- investment_calculator/investment_calculator.go
+++ investment_calculator/investment_calculator.go
@@ -17,18 +17,37 @@ func main() {
 	var years float64
 	var expectedReturnRate float64
 
-	fmt.Print("Investment Amount: ")
+	//fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Years: ")
+	//fmt.Print("Years: ")
+	outputText("Years: ")
 	fmt.Scan(&years)
 
-	fmt.Print("Expected Return Rate: ")
+	// fmt.Print("Expected Return Rate: ")
+	outputText("Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	// fmt.Println("Future Value", futureValue)
+	// fmt.Println("Future Real Value", futureRealValue)
+	// fmt.Printf("Future Value: %.1f\nFuture Real Value: %.0f", futureValue, futureRealValue)
+
+	// Using Sprint when storing values in variables
+	// formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
+	// formattedFRV := fmt.Sprintf("Future Real Value: %.0f", futureRealValue)
+	// fmt.Print(formattedFV, formattedFRV)
+
+	// Print using multi lines with back tick
+	fmt.Printf(`
+	Future Value: %.1f
+	Future Real Value: %.0f
+	`, futureValue, futureRealValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
 }
