0b2e13b9665c810c796faa64e605666b3346fb47
onis20 <emem.c.onis@gmail.com>
2025-02-25 16:39:31 +0200

    working with interfaces and generics on note-app

--- note_app/main.go
+++ note_app/main.go
@@ -7,10 +7,45 @@ import (
 	"strings"
 
 	"onis-emem.com/go/note-app/note"
+	"onis-emem.com/go/note-app/todo"
 )
 
+// Interfaces
+type saver interface {
+	Save() error
+}
+
+type displayer interface {
+	Display()
+}
+
+// type outputtable interface {
+// 	Save() error
+// 	Display()
+// }
+
+// Embedded Interfaces that works with interfaces live saver or just the method like Display()
+type outputtable interface {
+	saver
+	Display()
+}
+
 func main() {
+
+	printSomething(1)
+	printSomething(1.5)
+	printSomething("Hello")
+
 	title, content := getNoteData()
+
+	todoText := getUserInput("Todo Content: ")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	userNote, err := note.New(title, content)
 
@@ -19,15 +54,69 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
+	// Display and save todo
+
+	err = outputData(todo)
 
 	if err != nil {
-		fmt.Println("Saving the note failed this time.")
 		return
 	}
 
+	// Display and Save notes
+
+	outputData(userNote)
+}
+
+func printSomething(value interface{}) {
+
+	intVal, ok := value.(int)
+
+	if ok {
+		fmt.Println("Integer:", intVal)
+		return
+	}
+
+	floatVal, ok := value.(float64)
+
+	if ok {
+		fmt.Println("Integer:", floatVal)
+		return
+	}
+
+	stringVal, ok := value.(string)
+
+	if ok {
+		fmt.Println("Integer:", stringVal)
+		return
+	}
+
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("Integer", value)
+	// case float64:
+	// 	fmt.Println("Float", value)
+	// case string:
+	// 	fmt.Println("String", value)
+	// }
+
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed this time.")
+		return err
+	}
+
 	fmt.Println("Your note has been saved successfully!")
+
+	return nil
 }
 
 func getNoteData() (string, string) {
